b62a060b3a64d9894ef78927a1b82266ea3d7e30
pradeep <pradeephr03@icloud.com>
2022-08-10 19:38:13 +0530

    implemented using kotlin flows

--- app/src/main/java/com/pradeep/connectivity/MainActivity.kt
+++ app/src/main/java/com/pradeep/connectivity/MainActivity.kt
@@ -2,10 +2,32 @@ package com.pradeep.connectivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.pradeep.connectivity.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var connectivityObserver: ConnectivityObserver
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        connectivityObserver=NetworkConnectivityObserver(applicationContext)
+
+        setContentView(binding.root)
+
+        connectivityObserver.observe().onEach {
+            binding.textView.text=it.toString()
+        }.launchIn(lifecycleScope)
+
+
+
     }
 }
